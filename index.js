e4469c3800ddceb025472287540f685aa29d4c84
abdallahaac <aceydesignz@gmail.com>
2022-10-07 10:54:38 -0400

    added expanded nav bar on click

---
+++ index.js
@@ -0,0 +1,23 @@
+// burger icon images - closed and open
+const openBurgerImg = document.getElementById("burger-bar-img");
+const closedBurgerImg = document.getElementById("burger-bar-closed-img");
+
+// expanded burger navigation menu
+const expandedBurgerNav = document.getElementById("expanded-burger-nav");
+// try to rewrite as a switch case
+// switch burger icon image when clicked
+openBurgerImg.addEventListener("click", () => {
+	openBurgerImg.style.visibility = "hidden";
+	if ((openBurgerImg.style.visibility = "hidden")) {
+		closedBurgerImg.style.visibility = "visible";
+		expandedBurgerNav.style.visibility = "visible";
+	}
+
+	if ((closedBurgerImg.style.visibility = "visible")) {
+		closedBurgerImg.addEventListener("click", () => {
+			openBurgerImg.style.visibility = "visible";
+			closedBurgerImg.style.visibility = "hidden";
+			expandedBurgerNav.style.visibility = "hidden";
+		});
+	}
+});

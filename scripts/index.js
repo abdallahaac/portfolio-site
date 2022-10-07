64b4609684bf19715cda182e369e3e20ed953b5d
abdallahaac <aceydesignz@gmail.com>
2022-10-07 11:11:07 -0400

    added nav bar pages and stylesheets

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 // burger icon images - closed and open
 const openBurgerImg = document.getElementById("burger-bar-img");
 const closedBurgerImg = document.getElementById("burger-bar-closed-img");
-
 // expanded burger navigation menu
 const expandedBurgerNav = document.getElementById("expanded-burger-nav");
+
 // try to rewrite as a switch case
 // switch burger icon image when clicked
 openBurgerImg.addEventListener("click", () => {

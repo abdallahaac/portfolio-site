e327ca3dc41af3e8dd2c00d07e219bc0ef60d5aa
abdallahaac <aceydesignz@gmail.com>
2022-10-10 19:21:45 -0400

    added dropdown menu on desktop navbar

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,11 @@ const openBurgerImg = document.getElementById("burger-bar-img");
 const closedBurgerImg = document.getElementById("burger-bar-closed-img");
 // expanded burger navigation menu
 const expandedBurgerNav = document.getElementById("expanded-burger-nav");
+// portfolio btn on desktop navbar
+const portfolioNavBtn = document.getElementById("portfolio");
+//drop down menu that is hidden by default
+const dropdownMenu = document.getElementById("dropdown-menu");
 
-// try to rewrite as a switch case
 // switch burger icon image when clicked
 openBurgerImg.addEventListener("click", () => {
 	openBurgerImg.style.visibility = "hidden";
@@ -18,6 +21,30 @@ openBurgerImg.addEventListener("click", () => {
 			openBurgerImg.style.visibility = "visible";
 			closedBurgerImg.style.visibility = "hidden";
 			expandedBurgerNav.style.visibility = "hidden";
+		});
+	}
+});
+
+// make the drop down visible when the mouse is over the portfolio nav btn
+portfolioNavBtn.addEventListener("mouseover", () => {
+	dropdownMenu.style.visibility = "visible";
+
+	if ((dropdownMenu.style.visibility = "visible")) {
+		portfolioNavBtn.addEventListener("mouseleave", () => {
+			dropdownMenu.style.transitionDelay = "100ms";
+			dropdownMenu.style.visibility = "hidden";
+		});
+	}
+
+	if ((dropdownMenu.style.visibility = "visible")) {
+		dropdownMenu.addEventListener("mouseover", () => {
+			dropdownMenu.style.visibility = "visible";
+		});
+	}
+	if ((dropdownMenu.style.visibility = "visible")) {
+		dropdownMenu.addEventListener("mouseleave", () => {
+			dropdownMenu.style.transitionDelay = "100ms";
+			dropdownMenu.style.visibility = "hidden";
 		});
 	}
 });
